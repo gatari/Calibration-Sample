f002f2bfd18570c4cdbc17ba2b6fa657d4038c2f
yasu55k <yshdhiro@gmail.com>
2021-05-14 18:36:12 +0900

    first commit

---
+++ Assets/PointSetsMatrixEstimator.cs
@@ -0,0 +1,71 @@
+using MathNet.Numerics.LinearAlgebra;
+
+namespace DefaultNamespace
+{
+    public class PointSetsMatrixEstimator
+    {
+        private readonly Matrix<double> _pointSetsB;
+        private readonly Matrix<double> _pointSetsAd;
+        private readonly Matrix<double> _translationMat;
+        private readonly Matrix<double> _rotationMat;
+        private readonly Matrix<double> _replacementMat;
+
+
+        public PointSetsMatrixEstimator(double[,] pointSetsA, double[,] pointSetsB)
+        {
+            // 今回はAとBのサンプル数が同じな場合だけ
+            var n = pointSetsA.GetLength(1);
+            _translationMat = (1.0 / n) * Matrix<double>.Build.Dense(n, 1, 1.0) *
+                              Matrix<double>.Build.Dense(1, n, 1.0);
+            _pointSetsB = Matrix<double>.Build.DenseOfArray(pointSetsB);
+            _pointSetsAd = Matrix<double>.Build.DenseOfArray(pointSetsA) *
+                           (Matrix<double>.Build.DiagonalIdentity(n) - _translationMat);
+            var pointSetsBd = _pointSetsB * (Matrix<double>.Build.DiagonalIdentity(n) - _translationMat);
+            var svdA = _pointSetsAd.Svd(true);
+            var svdB = pointSetsBd.Svd(true);
+            _rotationMat = svdB.U * svdA.U.Transpose();
+            _replacementMat = svdA.VT.Transpose() * svdB.VT;
+        }
+
+        public (double[,] rotationMat, double[,] replacementMat) GetTransformationMatrixes()
+        {
+            return (_rotationMat.ToArray(), _replacementMat.ToArray());
+        }
+
+        public double[,] GetTransformedPositionsMatrix()
+        {
+            var b = _rotationMat * _pointSetsAd * _replacementMat + _pointSetsB * _translationMat;
+            return b.ToArray();
+        }
+
+        public double[,] GetRotatedPositionsMatrix()
+        {
+            var b = _rotationMat * _pointSetsAd * _replacementMat;
+            return b.ToArray();
+        }
+
+        // public double[,] GetTransformedPosition(double x, double y, double z)
+        // {
+        //     var pos = new double[,]
+        //     {
+        //         {x},
+        //         {y},
+        //         {z}
+        //     };
+        //     var b = _rotationMat * Matrix<double>.Build.DenseOfArray(pos) + _pointSetsB * _translationMat.Column(0).ToColumnMatrix();
+        //     return b.ToArray();
+        // }
+        //
+        // public double[,] GetRotatedPosition(double x, double y, double z)
+        // {
+        //     var pos = new double[,]
+        //     {
+        //         {x},
+        //         {y},
+        //         {z}
+        //     };
+        //     var b = _rotationMat * Matrix<double>.Build.DenseOfArray(pos);
+        //     return b.ToArray();
+        // }
+    }
+}
---
+++ Assets/RotationSample.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class RotationSample : MonoBehaviour
+    {
+        [SerializeField] private Transform[] a;
+        [SerializeField] private Transform[] b;
+
+
+        private void Start()
+        {
+            var aMat = GeneratePositionMatrix(a);
+            var bMat = GeneratePositionMatrix(b);
+
+            var estimator = new PointSetsMatrixEstimator(aMat, bMat);
+            var ans = estimator.GetTransformedPositionsMatrix();
+            Debug.Log(ConvertToUnityMatrix(ans));
+            for (int i = 0; i < a.Count(); i++)
+            {
+                a[i].position = new Vector3((float)ans[0, i], (float)ans[1, i], (float)ans[2, i]);
+            }
+            
+            var (r1, r2) = estimator.GetTransformationMatrixes();
+            Debug.Log(ConvertToUnityMatrix(r2));
+        }
+
+        private Matrix4x4 ConvertToUnityMatrix(double[,] rawMatrix)
+        {
+            var unityMat = new Matrix4x4();
+            for (int i = 0; i < rawMatrix.GetLength(0); i++)
+            {
+                unityMat.SetRow(i,
+                    new Vector4((float) rawMatrix[i, 0], (float) rawMatrix[i, 1], (float) rawMatrix[i, 2], 0));
+            }
+
+            unityMat.SetRow(3, new Vector4(0, 0, 0, 1));
+            return unityMat;
+        }
+
+        private double[,] GeneratePositionMatrix(IEnumerable<Transform> transforms)
+        {
+            var tfList = transforms.ToList();
+            var posMatrix = new double[3, tfList.Count()];
+            for (int i = 0; i < tfList.Count(); i++)
+            {
+                var pos = tfList[i].position;
+                posMatrix[0, i] = pos.x;
+                posMatrix[1, i] = pos.y;
+                posMatrix[2, i] = pos.z;
+            }
+
+            return posMatrix;
+        }
+    }
+}
